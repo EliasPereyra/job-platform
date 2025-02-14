ea81f64be0e5e77ab683f34783a34695da173051
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-02-13 22:16:31 -0300

    Change the font family to Raleway

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { draftMode } from "next/headers";
-import { Inter } from "next/font/google";
+import { Raleway } from "next/font/google";
 
 import "@/app/globals.css";
 import styles from "./layout.module.css";
@@ -7,7 +7,7 @@ import styles from "./layout.module.css";
 import { PreviewNotice } from "@/components/Globals/PreviewNotice/PreviewNotice";
 import Navigation from "@/components/Globals/Navigation/Navigation";
 
-const inter = Inter({ subsets: ["latin"] });
+const raleway = Raleway({ subsets: ["latin"] });
 
 export default async function RootLayout({
   children,
@@ -18,7 +18,7 @@ export default async function RootLayout({
 
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={raleway.className}>
         {isEnabled && <PreviewNotice />}
         <div className={styles.layout}>
           <Navigation />
