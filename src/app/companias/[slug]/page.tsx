152e7d88b11ab3a443cf1fa5710672d2cbaa714e
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-02-13 22:22:20 -0300

    Create a dynamic company page

---
+++ src/app/companias/[slug]/page.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+import { company } from "@/queries/general/CompanyQuery";
+import { type Company } from "@/gql/graphql";
+import { print } from "graphql/language/printer";
+
+import { fetchGraphQL } from "@/utils/fetchGraphQL";
+import styles from "./Company.module.css";
+
+export default async function Company({ params }: any) {
+  const { slug } = await params;
+  const { company: companyDetails } = await fetchGraphQL<{ company: Company }>(
+    print(company),
+    {
+      id: `/companias/${slug}`,
+    }
+  );
+
+  return (
+    <div className={styles.container}>
+      <Image
+        src={companyDetails.companies?.logo?.node.sourceUrl || ""}
+        alt={companyDetails.companies?.logo?.node.altText || ""}
+        width={200}
+        height={200}
+      />
+      <h1>{companyDetails.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: companyDetails.content || "" }} />
+    </div>
+  );
+}
