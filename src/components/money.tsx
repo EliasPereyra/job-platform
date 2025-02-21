5162c882fef43543c8c2f0d3b3a53aafcd7f87f7
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-02-20 22:46:43 -0300

    Create a styled component for showing the money amount

---
+++ src/components/money.tsx
@@ -0,0 +1,20 @@
+import { MoneyIcon } from "./icons/money";
+
+import styles from "./money.module.css";
+
+export default function Money({
+  color = "#c7ffc7",
+  salary,
+}: {
+  color?: string;
+  salary: string;
+}) {
+  return (
+    <div className={styles.salary}>
+      <MoneyIcon color={color} arialabel="Icono de dinero" />
+      <p style={{ color: color }} className={styles.salaryText}>
+        {salary}
+      </p>
+    </div>
+  );
+}
