af6cb0f1c1f4a8417b7ae3fdef6a36cb3a958ad2
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-02-24 18:39:53 -0300

    Fix: add missing fields from the graphql query

--- src/components/Templates/Page/JobsTemplate.tsx
+++ src/components/Templates/Page/JobsTemplate.tsx
@@ -1,9 +1,9 @@
 import { print } from "graphql/language/printer";
-import { RootQueryToJobCategoryConnection } from "@/gql/graphql";
+import { RootQueryToJobConnection } from "@/gql/graphql";
 import { fetchGraphQL } from "@/utils/fetchGraphQL";
-
-import JobCard from "@/components/job-card";
 import gql from "graphql-tag";
+
+import FilterJobs from "@/components/filter-jobs";
 
 import styles from "./Jobs.module.css";
 
@@ -21,13 +21,26 @@ const jobs = gql`
           salary
           available
           company {
-            nodes {
-              slug
+            node {
+              companies {
+                logo {
+                  node {
+                    altText
+                    sourceUrl
+                    mediaDetails {
+                      width
+                      height
+                    }
+                  }
+                }
+              }
             }
           }
-          jobcategory {
+          jobCategories {
             nodes {
-              slug
+              jobCategories {
+                name
+              }
             }
           }
         }
@@ -38,30 +51,12 @@ const jobs = gql`
 
 export default async function JobsTemplate() {
   const queryAllJobs = await fetchGraphQL<{
-    jobs: RootQueryToJobCategoryConnection;
+    jobs: RootQueryToJobConnection;
   }>(print(jobs));
-  const allJobs = queryAllJobs.jobs.nodes;
 
   return (
     <section className={styles.container}>
-      <h2 className={styles.title}>Últimos trabajos publicados</h2>
-      <ul className={styles.page}>
-        {allJobs.map((job: any) => (
-          <JobCard
-            key={job.content}
-            slug={job.slug}
-            logo={job.jobs.company.nodes.slug}
-            title={job.title}
-            publishDate={job.modified}
-            company={job.jobs.company.nodes.slug}
-            location={job.jobs.location}
-            modality={job.jobs.modality}
-            jobTime={job.jobs.jobtime}
-            salary={job.jobs.salary}
-            available={job.jobs.available}
-          />
-        ))}
-      </ul>
+      <FilterJobs jobs={queryAllJobs} />
     </section>
   );
 }
