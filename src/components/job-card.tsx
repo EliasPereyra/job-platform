47d884181ea5ae9e3cd52a8bb05d5898118f7d4a
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-02-27 23:46:50 -0300

    Fix: add the missing company name and redesign the content

--- src/components/job-card.tsx
+++ src/components/job-card.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import JobAvailable from "./job-available";
 import Location from "./location";
@@ -8,47 +9,74 @@ import { formatDate } from "@/utils/formatDate";
 
 import styles from "./job-card.module.css";
 
+interface JobCompanyLogoDetails {
+  sourceUrl: string;
+  altText: string;
+  mediaDetails: {
+    width: number;
+    height: number;
+  };
+}
+
 interface JobCardProps {
   slug: string;
-  logo?: string | undefined;
   title: string;
+  companyName: string;
   publishDate: string;
-  company: string;
+  companyLogo: JobCompanyLogoDetails;
   location: string;
   modality: string;
   jobTime: string;
   salary: number;
   available: boolean;
-  jobCategory: string;
+  jobCategories: string[];
 }
 
 export default function JobCard({
   slug,
-  logo,
   title,
-  company,
+  companyLogo,
+  companyName,
   publishDate,
   location,
   modality,
   jobTime,
   salary,
   available,
-  jobCategory,
+  jobCategories,
 }: JobCardProps) {
   return (
     <li className={styles.jobCardContainer} key={slug}>
       <Link href={`/job/${slug}`} className={styles.jobCard}>
-        <div className={styles.cardBg}>
-          <div className={styles.logo} />
-          <JobAvailable available={available} />
-        </div>
+        <div className={styles.cardBg}></div>
         <div className={styles.content}>
-          <h3>{title}</h3>
+          <div className={styles.company}>
+            <div className={styles.companyInfo}>
+              <Image
+                src={companyLogo?.sourceUrl}
+                alt={companyLogo?.altText}
+                width={companyLogo?.mediaDetails.width}
+                height={companyLogo?.mediaDetails.height}
+                className={styles.logo}
+              />
+              <div className={styles.companyContent}>
+                <h3 className={styles.jobTitle}>{title}</h3>
+                <small className={styles.companyName}>{companyName}</small>
+              </div>
+            </div>
+            <JobAvailable available={available} />
+          </div>
 
           <small className={styles.publishDate}>
-            {formatDate(publishDate)}
+            Publicado el {formatDate(publishDate)}
           </small>
-          <p className={styles.jobCategory}>{jobCategory}</p>
+          <ul className={styles.jobCategories}>
+            {jobCategories.map((category: any) => (
+              <li key={category.jobCategories.name}>
+                <small>#{category.jobCategories.name}</small>
+              </li>
+            ))}
+          </ul>
           <div className={styles.info}>
             <Location color="#333" location={location} />
             <Modality color="#333" modality={modality} />
