b5a58ba4e39b138c6cdeaa5e4a11fbe62baf0344
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-03-04 17:21:44 -0300

    Fix: add the company's contact email field

--- src/app/job/[slug]/page.tsx
+++ src/app/job/[slug]/page.tsx
@@ -1,23 +1,22 @@
 import Image from "next/image";
 import { Metadata } from "next";
 import Link from "next/link";
-import { Company, type Job } from "@/gql/graphql";
+import { type Job } from "@/gql/graphql";
 import gql from "graphql-tag";
 import { print } from "graphql/language/printer";
 
+import Location from "@/components/location";
+import Time from "@/components/time";
+import Money from "@/components/money";
+import Modality from "@/components/modality";
 import JobAvailable from "@/components/job-available";
-import { Location } from "@/components/icons/location";
-import WorldIcon from "@/components/icons/world";
-import { MoneyIcon } from "@/components/icons/money";
-import { TimeIcon } from "@/components/icons/time";
 import { WhatsappIcon } from "@/components/icons/whatsapp";
 import { FacebookIcon } from "@/components/icons/facebook";
 import { InstagramIcon } from "@/components/icons/instagram";
 import { LeftArrow } from "@/components/icons/left-arrow";
+import { Date } from "@/components/date";
 import { setSeoData } from "@/utils/seoData";
-import { formatDate } from "@/utils/formatDate";
 import { fetchGraphQL } from "@/utils/fetchGraphQL";
-import { company } from "@/queries/general/CompanyQuery";
 
 import styles from "./job.module.css";
 
@@ -31,13 +30,28 @@ const job = gql`
         available
         benefits
         company {
-          nodes {
+          node {
             slug
+            companies {
+              contact
+              logo {
+                node {
+                  altText
+                  sourceUrl
+                  mediaDetails {
+                    width
+                    height
+                  }
+                }
+              }
+            }
           }
         }
-        jobcategory {
+        jobCategories {
           nodes {
-            slug
+            jobCategories {
+              name
+            }
           }
         }
         contact
@@ -102,12 +116,6 @@ export default async function JobPage({ params }: Props) {
   const { job: jobDetails } = await fetchGraphQL<{ job: Job }>(print(job), {
     id: `/job/${slug}`,
   });
-  const { company: companyDetails } = await fetchGraphQL<{ company: Company }>(
-    print(company),
-    {
-      id: `/companias/${jobDetails.jobs?.company?.nodes[0].slug}`,
-    }
-  );
 
   const { title, content, modified, jobs } = jobDetails;
 
@@ -118,11 +126,11 @@ export default async function JobPage({ params }: Props) {
           <LeftArrow color="#fff" arialabel="Icono de flecha a la izquierda" />
         </Link>
         <div className={styles.company}>
-          <Link href={`/companias/${jobDetails.jobs?.company?.nodes[0].slug}`}>
+          <Link href={`/companias/${jobDetails.jobs?.company?.node.slug}`}>
             <Image
               className={styles.companyLogo}
-              src={companyDetails.companies?.logo?.node.sourceUrl || ""}
-              alt={companyDetails.companies?.logo?.node.altText || ""}
+              src={jobs?.company?.node.companies?.logo?.node.sourceUrl || ""}
+              alt={jobs?.company?.node.companies?.logo?.node.altText || ""}
               width={40}
               height={40}
             />
@@ -132,31 +140,21 @@ export default async function JobPage({ params }: Props) {
           <JobAvailable available={jobs?.available || false} />
         </div>
         <h3 className={styles.title}>{title}</h3>
-        <small className={styles.date}>{formatDate(modified || "")}</small>
+        <Date color="#fff" modified={modified || ""} />
         <div className={styles.info}>
-          <div className={styles.location}>
-            <Location color="#fff" arialabel="Icono de ubicacion" />
-            <p className={styles.locationText}>{jobs?.location}</p>
-          </div>
-          <div className={styles.modality}>
-            <WorldIcon color="#fff" arialabel="Icono de internet" />
-            <p className={styles.modalityText}>{jobs?.modality}</p>
-          </div>
-          <div className={styles.salary}>
-            <MoneyIcon color="#fff" arialabel="Icono de dinero" />
-            <p className={styles.salaryText}>{jobs?.salary}</p>
-          </div>
-          <div className={styles.time}>
-            <TimeIcon color="#fff" arialabel="Icono de reloj" />
-            <p className={styles.timeText}>
-              <strong>{jobs?.jobtime}</strong>
-            </p>
-          </div>
+          <Location location={jobs?.location || ""} />
+          <Modality modality={jobs?.modality || ""} />
+          <Money salary={jobs?.salary || ""} />
+          <Time jobtime={jobs?.jobtime || ""} />
         </div>
         <h4 className={styles.shareTitle}>Compartir</h4>
         <div className={styles.share}>
           <Link href="#">
-            <WhatsappIcon size={30} color="#fff" arilabel="Icono de whatsapp" />
+            <WhatsappIcon
+              size={30}
+              color="#fff"
+              arialabel="Icono de whatsapp"
+            />
           </Link>
           <Link href="#">
             <FacebookIcon
@@ -220,6 +218,19 @@ export default async function JobPage({ params }: Props) {
                 <li key={index}>{benefit}</li>
               ))}
           </ul>
+        </div>
+        <div className={styles.contact}>
+          <h3>Contacta a la empresa</h3>
+          <p>¿Te ha gustado el trabajo y quieres contactar con la empresa?</p>
+          <p>
+            Envía un correo a:{" "}
+            <a
+              className={styles.link}
+              href={`mailto:${jobs?.company?.node.companies?.contact}`}
+            >
+              <strong>{jobs?.company?.node.companies?.contact}</strong>
+            </a>
+          </p>
         </div>
       </section>
     </div>
