59210ef5f2669c2e311c09f131aad9fd2a2b87cf
EliasPereyra <eliaspereyra_gomez@hotmail.com>
2025-03-03 23:23:04 -0300

    Fix: ui improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import { Metadata } from "next";
 import Link from "next/link";
 import Image from "next/image";
-import { companies } from "@/queries/general/CompanyQuery";
 import { print } from "graphql/language/printer";
-import { RootQueryToCompanyConnection } from "@/gql/graphql";
 
 import Navigation from "@/components/Globals/Navigation/Navigation";
-import Footer from "@/components/footer";
+import Footer from "@/components/Globals/Footer/footer";
+import { companies } from "@/queries/general/CompanyQuery";
+import { RootQueryToCompanyConnection } from "@/gql/graphql";
 import { fetchGraphQL } from "@/utils/fetchGraphQL";
 
 import styles from "./home.module.css";
@@ -33,11 +33,11 @@ export default async function Home() {
           <span className={styles.highlight}>sin experiencia</span>
         </h1>
         <p className={styles.description}>
-          Disponibles desde distintas provincias y localidades, comienza tu
-          próxima trayectoria laboral
+          Explora oportunidades laborales y contacta directamente con las
+          empresas. Sin registros, sin intermediarios
         </p>
         <a className={styles.button} href="/todos-los-trabajos">
-          Ver trabajos
+          Ver Ofertas de Trabajos
         </a>
 
         <div className={styles.argentina} />
@@ -62,37 +62,41 @@ export default async function Home() {
         </div>
       </section>
 
-      <section className={styles.firstJob}>
+      <section className={styles.firstJobContainer}>
         <h2 className={styles.firstJobTitle}>
           Conseguí tu{" "}
           <span className={styles.firstJobTitleHighlight}>primer empleo</span>
         </h2>
         <div className={styles.firstJobContent}>
           <div className={styles.firstColumn}>
-            <div className={styles.leftRectangleRounded} />
             <p className={styles.columnText}>
-              Creemos que todos tienen derecho a trabajar, y para eso es
-              fundamental pasar por un primer empleo que nos permita empezar a
-              tener experiencia.
+              Encontrar el primer empleo puede ser un desafío, pero creemos que
+              todos merecen la oportunidad de dar ese primer paso en el mundo
+              laboral.
             </p>
             <p className={styles.columnText}>
-              Por eso, en 2016, fundamos Revista Empleo con la misión de sólo
-              publicar ofertas de trabajo que no requieren experiencia previa.
+              Esta es una plataforma que facilita el acceso a empleos sin
+              experiencia previa, conectando a postulantes con empresas,
+              organismos públicos y emprendedores/as que buscan talento nuevo. A
+              través de nuestra plataforma, reunimos ofertas de trabajo
+              accesibles para quienes están comenzando y brindamos herramientas
+              para prepararte mejor en tu búsqueda laboral.
+            </p>
+            <p className={styles.columnText}>
+              Nuestro compromiso es derribar la barrera de la falta de
+              experiencia. Sabemos que empezar no siempre es fácil, por eso
+              ofrecemos un espacio donde las oportunidades son reales y pensadas
+              para quienes están dando sus primeros pasos en el mercado laboral.
+            </p>
+
+            <p className={styles.columnText}>
+              Más que una bolsa de trabajo, somos un punto de partida. Lo que
+              más nos motiva es saber que vos, que hoy estás buscando tu primera
+              oportunidad, puedas encontrarla y empezar a construir tu futuro
+              laboral.
             </p>
           </div>
-          <div className={styles.secondColumn}>
-            <p className={styles.columnText}>
-              Miles de empresas, organismos públicos y emprendedores/as nos
-              acompañan en este camino y nos eligen para publicar sus búsquedas
-              de personal. ¡Y estamos felices de ver cómo se suman cada día más
-              compañías a este proyecto!
-            </p>
-            <p className={styles.columnText}>
-              Pero lo que más alegría nos da es que vos, que estás buscando
-              trabajo, lo consigas pronto.
-            </p>
-            <div className={styles.rightRectangleRounded} />
-          </div>
+          <div className={styles.secondColumn}></div>
         </div>
       </section>
 
@@ -107,7 +111,8 @@ export default async function Home() {
         </div>
         <div className={styles.secondCol}>
           <h2 className={styles.secondJobTitle}>
-            Revista Empleo arma tu CV y mucho más
+            <span className={styles.secondJobTitleHighlight}>WorkStart</span>{" "}
+            arma tu CV y mucho más
           </h2>
           <div className={styles.secondJobContent}>
             <p className={styles.secondJobDescription}>
